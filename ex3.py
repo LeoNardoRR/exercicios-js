a3f2188b7f249e92e2283895cc3f4c335ed31af8
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-04-27 07:58:08 -0300

    Update

---
+++ ex3.py
@@ -0,0 +1,12 @@
+#Escreva uma função que recebe um vetor de números e escreva no console quais números aparecem mais de uma 
+#vez e a quantidade de repetições. Exemplo:
+
+def repeticao (list):
+
+    salvo = []
+
+    for i in list:
+        num = int(input('Digite um número: '))
+        if 
+        salvo.append(num)
+    
