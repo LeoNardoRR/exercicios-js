35e399e99ff5c998950b9da23f1257a39bbbd7e3
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-05-23 23:08:12 -0300

    update

---
+++ Lista-algoritmos/ex20.js
@@ -0,0 +1,17 @@
+// 20) Desenvolva um programa que leia um número inteiro e mostre se ele é PAR ou 
+// ÍMPAR
+const prompt = require('prompt-sync')({sigint: true})
+
+let num = parseInt(prompt('Digite um número: '))
+
+if(num / 2 == 0)
+{
+    console.log(`O número digitado é par`)
+}
+else if (num / 2 ==1)
+{
+    console.log(`O número digitado é impar`)
+}
+else{
+    console.log(`Valor invalido`)
+}
