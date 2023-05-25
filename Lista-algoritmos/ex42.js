de757620ff3bbf299268bca6c3f477dfd5792eea
LeoNardoRR <ribeiroleonardoti@gmail.com>
2023-05-25 11:16:04 -0300

    update js

--- Lista-algoritmos/ex42.js
+++ Lista-algoritmos/ex42.js
@@ -2,3 +2,17 @@
 // qualquer e mostre uma contagem até esse valor:
 // Ex: Digite um valor: 35
 // Contagem: 1 2 3 4 5 6 7 ... 33 34 35 Acabou!
+
+const prompt = require('prompt-sync')();
+
+// Solicita ao usuário um número
+const numero = parseInt(prompt('Digite um número: '));
+
+let contador = 0;
+
+while (contador <= numero) {
+  console.log(contador);
+  contador++;
+}
+
+console.log("Acabou!");
---
+++ Lista-algoritmos/ex43 - Ajustar.js
@@ -0,0 +1,22 @@
+// 43) Desenvolva um algoritmo que mostre uma contagem regressiva de 30 até 1, 
+// marcando os números que forem divisíveis por 4, exatamente como mostrado abaixo:
+// 30 29 [28] 27 26 25 [24] 23 22 21 [20] 19 18 17 [16]..
+
+const prompt = require('prompt-sync')();
+
+let contador = 30;
+let resultado = '';
+
+while (contador >= 0) {
+  resultado += contador + ' ';
+  contador--;
+
+  if (contador % 4 === 0) {
+    resultado += `[${contador}] `;
+  } else {
+    console.log();
+  }
+}
+
+console.log(resultado.trim()); // Imprime o resultado em uma linha
+console.log("Acabou");
